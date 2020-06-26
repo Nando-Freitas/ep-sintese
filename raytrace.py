8ad7fedaa7cc0e75c84fd6643ff5b3f7c7edabf4
deh410 <aearahawa@gmail.com>
2020-06-26 18:47:01 -0300

    implementa intersecao com o plano

--- raytrace.py
+++ raytrace.py
@@ -6,18 +6,25 @@ import matplotlib.pyplot as plt
 # A ideia eh fazer um raytracing bem simples, em python single thread mesmo usando esferas e um plano
 
 
-def intersect_plane(O, D, P, N):
+def intersecao_plano(O, D, P, N):
     # Retorna a distancia do Objeto O a intersecao do raio (Objeto, Direcao) com o Plano (Plano, Normal), ou +infinito se nao ha intersecao
     # Objeto e Plano sao pontos 3D, Direcao e Normal sao vetores normalizados
+    denominador = np.dot(D, N)
+    if np.abs(denominador) < 1e-6:
+        return np.inf
+    distancia = np.dot(P - O, N) / denominador
+    if distancia < 0:
+        return np.inf
+    return distancia
 
 
-def intersect_sphere(O, D, S, R):
+def intersecao_esfera(O, D, S, R):
     # Bem parecido com o do plano, mas para esfera é um pouco mais simples porque da pra saber se há intersecao baseado no raio
     # Retorna a distancia do Objeto O ate a intersecao do raio (Objeto, Direcao) com a esfera (Esfera S, Raio), ou mais infinito se nao ha intersecao
     # Objeto e Esfera S sao pontos 3D, Direcao eh um vetor normalizado, Raio R é um escalar
 
 
-def trace_ray(rayO, rayD):
+def tracar_raio(rayO, rayD):
   # Encontra o primeiro ponto de interesao com a cena
   # Retorna None se o raio nao intercepta nenhum objeto
   # Encontra o objeto
